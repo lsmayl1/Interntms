ec9322968b58a8c9a1b65b1c0c07710fa4ae8109
ismayil.ismayilov <ismayilovi925@mail.com>
2025-04-13 21:33:23 +0400

    13/04

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const categoriesRoutes = require("./routes/categoriesRoutes");
 const testRoutes = require("./routes/testRoutes"); // Import the test routes
 const errorHandler = require("./middleware/errorMiddleware"); // Import the error handling middleware
 require("dotenv").config(); // Load environment variables from .env file
-
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 // Middleware to parse JSON requests
 app.use(express.json());
 
@@ -31,15 +31,23 @@ syncDatabase()
   });
 
 // Use routes
-app.use("/interns", internRoutes);
-app.use("/tasks", taskRoutes);
-app.use("/auth", authRoutes);
-app.use("/categories", categoriesRoutes);
-app.use("/test", testRoutes); // Use the test routes
+app.use("/api/interns", internRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/categories", categoriesRoutes);
+app.use("/api/test", testRoutes); // Use the test routes
 
 // Use the error handling middleware
 app.use(errorHandler);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./dist")));
+
+// Tüm istekleri index.html'e yönlendir (React Router için)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./dist/", "index.html"));
+});
 // Start the server
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
